096c7d25c0d927dd553bc28bcf3093dedcf1e342
K0ndrich <123kondrich@gmail.com>
2024-08-12 18:00:11 +0300

    edited

---
+++ MAIN/Cyber Security/Windows/Отключаем ненужное (как Администратор)/Отключаем потенциально уязвимые службы/potentially.bat
@@ -0,0 +1,10 @@
+net stop RemoteRegistry
+sc config RemoteRegistry start=disabled
+net stop TermService
+sc config TermService start=disabled
+net stop TrkWks
+sc config TrkWks start=disabled
+net stop DPS
+sc config DPS start=disabled
+
+pause
